7702612f720f90f5f22ee4a79bfe1598f71e2a7d
ChunPhally <cphally.student@puthisastra.edu.kh>
2024-08-06 00:31:05 +0700

    Update app container

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 import { Box, ThemeProvider } from '@mui/material';
 import customTheme from './them';
 import React from 'react';
-// import Sidebar from './layouts/sidebar';
 import RouteContainer from './routes';
 
 
 const App = () => {
-  console.log('location==>', location);
   return (
     <ThemeProvider theme={customTheme}>
       <Box bgcolor='white' width='100%' height='100%'>
