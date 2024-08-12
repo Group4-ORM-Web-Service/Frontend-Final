fd7001118983cec9912aada64ddca9c0dee962c3
ChunPhally <cphally.student@puthisastra.edu.kh>
2024-08-12 15:19:35 +0700

    Update user account page

--- src/screens/userProfile/index.js
+++ src/screens/userProfile/index.js
@@ -1,52 +1,117 @@
-import React, {useState, useEffect} from 'react';
-import { Box, Typography, Paper, Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Grid, Paper, TextField, Typography, Button, Box, Container } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
 import Sidebar from '../../layouts/sidebar';
-const STORAGE_KEY = 'USER_DATA';
+import { STORAGE_KEY } from '../../constant/keyComponent';
+import { useAuth } from '../../context/AuthContext';
 
 const UserProfile = () => {
-  const [userData, setUserData] = useState(null);
+  const { logout } = useAuth();
+  const [user, setUser] = useState(null);
+
+  const handleLogout = React.useCallback(() => {
+    logout();
+  }, [logout]);
 
   useEffect(() => {
-    // const storedData = localStorage.getItem(STORAGE_KEY);
-    const storedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
+    const storedData = JSON.parse(localStorage.getItem(STORAGE_KEY.USER_DATA));
     if (storedData) {
-      setUserData(JSON.parse(storedData));
+      setUser(storedData);
     } else {
       console.log('Data not found in local storage.');
-      // Handle the case when data is not found in local storage
     }
   }, []);
 
-  console.log(userData);
-
-  // if (!userData) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // const { name, email } = userData;
   return (
-    <Box bgcolor='grey.50'>
-      <Sidebar pageName='UserProfile' />
-      <Paper elevation={3} style={{ padding: 20, margin: 20 }}>
-        <Typography variant="h5" gutterBottom>User Profile</Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="body1"><strong>Name:</strong></Typography>
+    <>
+      <Sidebar pageName='Profile' />
+      <Container
+        maxWidth='lg'
+        style={{
+          height: window.innerHeight,
+          paddingTop: '64px',
+        }}
+      >
+        <Grid container spacing={3} sx={{ bgcolor: 'grey.50' }}>
+          <Grid item xs={12} md={4} sx={{ bgcolor: 'grey.50' }}>
+            <Paper elevation={3} sx={{ padding: 2, backgroundColor: 'white' }}>
+              <Box display='flex' flexDirection='column' alignItems='center'>
+                <AccountCircle style={{ fontSize: 80, color: '#311b92' }} />
+                <Typography variant='h6' gutterBottom color='blue.900' fontWeight='500'>
+                  {user?.username || 'N/A'}
+                </Typography>
+              </Box>
+              <Box mt={2}>
+                <Typography variant='body2'>User ID: {`CU-${user?.user_id}` || 'N/A'}</Typography>
+                <Typography variant='subtitle1' gutterBottom my='16px'>
+                  Role: {user?.role === 'User' ? 'Customer' : 'Employee'}
+                </Typography>
+                <Typography variant='body2' my='16px'>
+                  Register On: {new Date(user?.createdAt).toLocaleString()}
+                </Typography>
+              </Box>
+              <Button
+                variant='contained'
+                color='error'
+                sx={{ marginTop: 2 }}
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </Paper>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body1"><strong>Email:</strong></Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body1">
-              {/* <strong>Roles:</strong> {roles.map((role, index) => (
-                <span key={index}>{role}{index !== roles.length - 1 ? ', ' : ''}</span>
-              )}; */}
-            </Typography>
+          <Grid item xs={12} md={8} sx={{ bgcolor: 'grey.50' }}>
+            <Paper elevation={3} sx={{ padding: 2, backgroundColor: 'white' }}>
+              <Typography variant='h5' gutterBottom mb='32px' color='blue.900' fontWeight='600'>
+                Account
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Username' fullWidth value={user?.username || 'N/A'} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Email' fullWidth value={user?.email || 'N/A'} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    label='Password'
+                    type='password'
+                    fullWidth
+                    value={user?.password || 'N/A'}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Role' fullWidth value={user?.role || 'N/A'} />
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant='h6' gutterBottom>
+                    Customer Details
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Name' fullWidth value={user?.customer?.name || 'N/A'} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Address' fullWidth value={user?.customer?.address || 'N/A'} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='City' fullWidth value={user?.customer?.city || 'N/A'} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Country' fullWidth value={user?.customer?.country || 'N/A'} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Phone' fullWidth value={user?.customer?.phone || 'N/A'} />
+                </Grid>
+              </Grid>
+              <Button variant='contained' color='primary' sx={{ marginTop: 2 }}>
+                Edit
+              </Button>
+            </Paper>
           </Grid>
         </Grid>
-      </Paper>
-    </Box>
+      </Container>
+    </>
   );
 };
 
